6251895891df4278302f3bb37e389a916ed4dd3b
Ben Ockmore <ben.sput@gmail.com>
2015-07-08 15:49:43 +0100

    Initial commit - basic structure inc. tests, gitignore, LICENSE, README and testing config.

---
+++ README.rst
@@ -0,0 +1,17 @@
+peevee
+======
+
+A path validator and transformer, allowing for checking and modification of
+input path strings to make them safe for use on various platforms and
+filesystems.
+
+This is apparently lacking from the standard library, and doesn't seem
+available on PyPI. I've come across a couple of applications which need this
+functionality and hack a solution for it with varying degrees of success.
+Hopefully, this package will put together a more elegant and flexible
+solution.
+
+The name "peevee" comes from the initial letters of "Path" and "Validator",
+"PV", which is pronounced "peevee" in English! Also, it sounds similar to the
+verb "peeve", which is a verb describing how having to write platform-specific
+code makes developers feel!
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Ben Ockmore
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from distutils.core import setup
+
+setup(
+    name=u'peevee',
+    version=u'0.1',
+    description=u'A path validator and transformer',
+    author=u'Ben Ockmore',
+    author_email=u'ben.sput@gmail.com',
+    url=u'https://bitbucket.org/LordSputnik/peevee',
+    packages=[u'peevee'],
+    provides=[u'peevee']
+)
